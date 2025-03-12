d67f28c9cf0bd1a6447def7496c5b689754903b5
hardikcursor <cursorsoft776@gmail.com>
2025-03-12 14:44:44 +0530

    update

--- app/Http/Controllers/Admin/ResumeController.php
+++ app/Http/Controllers/Admin/ResumeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class ResumeController extends Controller
 {
     public function index() {
-        return view('admin.index');
+        return view('index');
     }
 }
