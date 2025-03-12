15cb68f34f85d097f8aa2e4b2edc340c691a2f16
hardikcursor <cursorsoft776@gmail.com>
2025-03-12 13:58:22 +0530

    first commit

---
+++ resources/views/include/script.blade.php
@@ -0,0 +1,11 @@
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('admin_assets/lib/easing/easing.min.js') }}"></script>
+<script src="{{ asset('admin_assets/lib/slick/slick.min.js') }}"></script>
+<script src="{{ asset('admin_assets/lib/typed/typed.min.js') }}"></script>
+<script src="{{ asset('admin_assets/lib/waypoints/waypoints.min.js') }}"></script>
+<script src="{{ asset('admin_assets/lib/isotope/isotope.pkgd.min.js') }}"></script>
+<script src="{{ asset('admin_assets/lib/lightbox/js/lightbox.min.js') }}"></script>
+
+<!-- Template Javascript -->
+<script src="{{ asset('admin_assets/js/main.js') }}"></script>
---
+++ resources/views/layout/admin.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('include.head')
+<body  data-spy="scroll" data-target=".navbar" data-offset="51">
+    @yield('main')
+    @include('include.script')
+</body>
+</html>
